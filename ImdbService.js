deeec5711193ac5c1cd5dc746f3deb4a08b3af1a
unknown <apetersen@LT0191.skyline.gb>
2014-02-12 23:28:51 -0600

    initial commit

---
+++ ImdbService.js
@@ -0,0 +1,21 @@
+var HttpService = require("./HttpService"),
+	q = require("q"),
+	cheerio = require("cheerio");
+
+var ImdbService = function() {
+	this.baseUrl = "http://www.imdb.com/title/";
+};
+
+ImdbService.prototype = new HttpService();
+
+ImdbService.prototype.getRating = function(imdbId) {
+	var deferred = q.defer();
+	this.get(this.baseUrl + imdbId).then(function(html){
+		var $ = cheerio.load(html);
+		var rating = $(".star-box-giga-star").text().trim();
+		deferred.resolve(rating);
+	});
+	return deferred.promise;
+};
+
+module.exports = ImdbService;
